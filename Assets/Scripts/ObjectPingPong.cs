8c69d8eb3a6315d26ef7c01a6796927bd327a27a
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-03 16:46:28 +0300

    reset

--- Assets/Scripts/ObjectPingPong.cs
+++ Assets/Scripts/ObjectPingPong.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine;
+
 public class ObjectPingPong : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float startPosition = 0f; // начальна€ позици€ объекта
+    public float targetPosition = 100f; // конечна€ позици€ объекта
+    public float duration = 1f; // врем€ перемещени€ (в секундах)
+    public float pauseDuration = 1f; // длительность паузы (в секундах)
+
+    private IEnumerator Start()
     {
-        
+
+        while (true)
+        {
+            // движение вправо
+            yield return LerpPosition(startPosition, targetPosition, duration);
+
+            // пауза
+            yield return new WaitForSeconds(pauseDuration);
+
+            // движение влево
+            yield return LerpPosition(targetPosition, startPosition, duration);
+
+            yield return new WaitForSeconds(pauseDuration);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator LerpPosition(float start, float end, float time)
     {
-        
+        float elapsedTime = 0f;
+
+        while (elapsedTime < time)
+        {
+            float t = elapsedTime / time;
+            transform.localPosition = new Vector3(Mathf.Lerp(start, end, t), transform.localPosition.y, transform.localPosition.z);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // ”становка окончательной позиции (это нужно, чтобы избежать небольшой погрешности)
+        transform.localPosition = new Vector3(end, transform.localPosition.y, transform.localPosition.z); ;
     }
 }
+
