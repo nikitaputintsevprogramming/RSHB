5e793bb274a93bb1745c49c54f015bb87b9bf920
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-03 16:03:37 +0300

    анимация 1 готова

--- Assets/Scripts/AnimationParts.cs
+++ Assets/Scripts/AnimationParts.cs
@@ -4,72 +4,81 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class AnimationParts : MonoBehaviour
+namespace UI.Pagination
 {
-    public Transform[] imageTransform; // Массив с позициями для каждой картинки
-    public Vector3[] imagePos; // Массив с позициями для каждой картинки
-    public float animationSpeed = 1f; // Скорость анимации
-    private int currentImageIndex; // Индекс текущей картинки
-    private bool isAnimating = false; // Флаг, указывающий на то, идет ли анимация
-
-    private void Start()
+    public class AnimationParts : MonoBehaviour
     {
-        imagePos = new Vector3[imageTransform.Length];
+        public RectTransform[] imageTransform; // Массив с позициями для каждой картинки
+        public Vector3[] imagePos; // Массив с позициями для каждой картинки
+        public float animationSpeed = 1f; // Скорость анимации
+        private int currentImageIndex; // Индекс текущей картинки
+        private bool isAnimating = false; // Флаг, указывающий на то, идет ли анимация
 
-        for (int i = 0; i < imageTransform.Length; i++)
+        [SerializeField] private GameObject _pageRect;
+        [SerializeField] private bool checkPos;
+
+        private void Start()
         {
-            imagePos[i] = imageTransform[i].position;
-        }
-    }
 
-    public void StartSystemPoints()
-    {
-        currentImageIndex = 0;
+            imagePos = new Vector3[imageTransform.Length];
 
-        // Устанавливаем все картинки в начальную позицию
-        for (int i = 0; i < imageTransform.Length; i++)
-        {
-            imageTransform[i].gameObject.SetActive(false);
-            imageTransform[i].position = imagePos[i];
+            for (int i = 0; i < imageTransform.Length; i++)
+            {
+                imagePos[i] = imageTransform[i].localPosition;
+            }
         }
 
-        isAnimating = true;
-        StartCoroutine(AnimateImages());
-        
-    }
-
-    private IEnumerator AnimateImages()
-    {
-        for (int i = 0; i < imageTransform.Length; i++)
+        public void StartSystemPoints()
         {
-            // Плавно перемещаем текущую картинку к следующей позиции
-            StartCoroutine(MoveImage(imageTransform[currentImageIndex], imageTransform[i]));
+            if (isAnimating)
+                return;
 
-            // Ожидаем завершения перемещения текущей картинки
-            yield return new WaitForSeconds(animationSpeed);
+            currentImageIndex = 0;
 
-            // Показываем следующую картинку и устанавливаем ее на место текущей
-            imageTransform[i].gameObject.SetActive(true);
-            imageTransform[currentImageIndex].position = imageTransform[i].position;
+            // Устанавливаем все картинки в начальную позицию
+            for (int i = 0; i < imageTransform.Length; i++)
+            {
+                imageTransform[i].gameObject.SetActive(false);
+                imageTransform[i].localPosition = imagePos[i];
+            }
 
-            // Переходим к следующей картинке
-            currentImageIndex = i;
+            isAnimating = true;
+            StartCoroutine(AnimateImages());
         }
 
-        isAnimating = false;
-    }
-
-    private IEnumerator MoveImage(Transform from, Transform to)
-    {
-        float elapsedTime = 0f;
-
-        while (elapsedTime < animationSpeed)
+        private IEnumerator AnimateImages()
         {
-            // Интерполируем позицию от from до to с помощью Lerp
-            from.position = Vector3.Lerp(from.position, to.position, elapsedTime / animationSpeed);
+            for (int i = 0; i < imageTransform.Length; i++)
+            {
+                // Плавно перемещаем текущую картинку к следующей позиции
+                StartCoroutine(MoveImage(imageTransform[currentImageIndex], imageTransform[i]));
 
-            elapsedTime += Time.deltaTime;
-            yield return null;
+                // Ожидаем завершения перемещения текущей картинки
+                yield return new WaitForSeconds(animationSpeed);
+
+                // Показываем следующую картинку и устанавливаем ее на место текущей
+                imageTransform[i].gameObject.SetActive(true);
+                imageTransform[currentImageIndex].position = imageTransform[i].position;
+
+                // Переходим к следующей картинке
+                currentImageIndex = i;
+            }
+
+            isAnimating = false;
+        }
+
+        private IEnumerator MoveImage(Transform from, Transform to)
+        {
+            float elapsedTime = 0f;
+
+            while (elapsedTime < animationSpeed)
+            {
+                // Интерполируем позицию от from до to с помощью Lerp
+                from.position = Vector3.Lerp(from.position, to.position, elapsedTime / animationSpeed);
+
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
